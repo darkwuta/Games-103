d70380f8aeca6eb079d8b3a7201751aad499f942
darkmon <z214971500@hotmail.com>
2021-11-16 23:01:17 +0800

    add transform

--- UnityAssignments/Assets/Scripts/RigidBodyDynamics.cs
+++ UnityAssignments/Assets/Scripts/RigidBodyDynamics.cs
@@ -8,6 +8,8 @@ public class RigidBodyDynamics : MonoBehaviour
     private Rigidbody rigidbody;
 
     [SerializeField]
+    private Vector3 force;
+    [SerializeField]
     private Vector3 velocity;
     [SerializeField]
     private Quaternion angularVelocity;
@@ -21,7 +23,7 @@ public class RigidBodyDynamics : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (this.GetComponent<Rigidbody>())
         {
@@ -47,12 +49,52 @@ public class RigidBodyDynamics : MonoBehaviour
         }
             
         else
-            Simulation();
+        {
+            // 上下左右
+            if (Input.GetKey(KeyCode.LeftArrow))
+                AddForce(new Vector3(-1, 0, 0) * MagOfForce);
+            if (Input.GetKey(KeyCode.RightArrow))
+                AddForce(new Vector3(1, 0, 0) * MagOfForce);
+            if (Input.GetKey(KeyCode.UpArrow))
+                AddForce(new Vector3(0, 1, 0) * MagOfForce);
+            if (Input.GetKey(KeyCode.DownArrow))
+                AddForce(new Vector3(0, -1, 0) * MagOfForce);
+
+            // 旋转 
+            if (Input.GetKey(KeyCode.R))
+                AddTorque(new Vector3(Mathf.Sin(Time.time), Mathf.Sin(Time.time), Mathf.Sin(Time.time)));
+
+            // 前后
+            if (Input.GetKey(KeyCode.Space))
+                AddForce(new Vector3(0, 0, 1) * MagOfForce);
+            if (Input.GetKey(KeyCode.LeftShift))
+                AddForce(new Vector3(0, 0, -1) * MagOfForce);
+
+            Simulate();
+        }
+            
 
     }
 
-    void Simulation()
+    void Simulate()
     {
+        velocity += force * Time.fixedDeltaTime;
+        transform.position += velocity * Time.fixedDeltaTime;
 
+        //Drag
+        //velocity *= 0.9f;
+        //力归零
+        force = new Vector3(0, 0, 0);
+    }
+
+    void AddForce(Vector3 f)
+    {
+        force = f;
+        force -= 2 * velocity;
+    }
+
+    void AddTorque(Vector3 torque)
+    {
+        
     }
 }
